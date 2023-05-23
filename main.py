03a3bf5501412df9fd92f979c391157154fc4792
davidtrueb <122185781+davidtrueb@users.noreply.github.com>
2023-05-23 04:21:27 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,109 @@
+# Trying to mimic the McDonalds Drive Through
+
+
+# Modules for project
+import math
+
+
+# SUBTOTAL BEGINNING = 0
+subtotal = 0
+
+
+special_menu = {
+    
+        "SPECIAL BURGER WITH FRIES": 15.99,
+        "SPECIAL ICE CREAM WITH LARG COLA": 7.99,
+        "SURPRISE MENU": 18.99
+        }
+
+
+regular_menu = {
+        
+        "LARG FRIES": 3.99,
+        "LARG CHEESE BURGER": 5.99,
+        "LARG DOPPLE WOPPER ROYAL": 11.99,
+        "SALAD W/ ONIONS AND CHICKEN": 11.99,
+        "LAVA CAKE WITH ICE CREAM": 6.99
+        }
+
+
+# IF ELSE STATEMENTS - AND PRICETAG OF EACH SPECIAL-MENU
+def special_menu_method():
+
+    # Start - DIFF. PRINT STATEMENTS
+    print("HELLO AND WELCOME TO MCDONALDS DRIVE-THROUGH\n")
+    print("------------------------------")
+    print("WHAT WOULD YOU LIKE TO ORDER?\n")
+    print("NR 01 - SPECIAL BURGER WITH FRIES --- 15.99")
+    print("NR 02 - SPECIAL ICE CREAM WITH LARG COLA --- 7.99")
+    print("NR 03 - SURPRISE MENU --- 18.99\n")
+
+    print("------------------------------")
+
+    # USER CHOOSES
+    user_input_special = int(input("CHOSE 1 - 3: "))
+
+    if user_input_special == 1:
+        print("NR 01 IS SELECTED")
+
+    elif user_input_special == 2:
+        print("NR 01 IS SELECTED")
+
+    elif user_input_special == 3:
+        print("NR 03 IS SELECTED")
+
+    else:
+       print("INVALID INPUT")
+            
+special_menu_method()
+
+print("------------------------------")
+
+# NEXT ORDER
+def regular_menu_method():
+    
+    # REGULAR MENU ITEMS 
+    print("NOW TO OUR REGULAR MENU ITEMS...\n")
+    print("NR 01 - LARG FRIES --- 4.99")
+    print("NR 02 - LARG CHEESE BURGER --- 5.99")
+    print("NR 03 - LARG DOPPLE WOPPER ROYAL --- 8.99")
+    print("NR 04 - SALAD W/ ONIONS AND CHICKEN --- 11.99")
+    print("NR 05 - LAVA CAKE WITH ICE CREAM --- 6.99\n")
+
+    print("------------------------------")
+
+    # USER CHOOSES
+    user_input_regular = int(input("CHOSE 1 - 5: "))
+    
+    if user_input_regular == 1:
+        print("NR 01 IS SELECTED")
+
+    elif user_input_regular == 2:
+        print("NR 02 IS SELECTED")
+
+    elif user_input_regular == 3:
+        print("NR 03 IS SELECTED")
+
+    elif user_input_regular == 4:
+        print("NR 04 IS SELECTED")
+
+    elif user_input_regular == 5:
+        print("NR 05 IS SELECTED")
+
+regular_menu_method()
+
+# SUBTOTAL
+while True:
+    special_menu_method = input("Enter the item (or 'done' to finish): ")
+    if special_menu_method == "done":
+        break
+
+    # Check if the item is in the menu
+    if user_input_regular in  special_menu:
+        # Add the price of the item to the subtotal
+        subtotal += special_menu[user_input_regular]
+    else:
+        print("Invalid item. Please try again.")
+
+# Print the subtotal
+print("Subtotal: $", subtotal)
